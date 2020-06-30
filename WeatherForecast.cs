1463f8961fe02f39ae997d208f91ffa36b2fcda3
xuxuzhaozhao <xuxuzhaozhao@qq.com>
2020-06-30 17:18:09 +0800

    init

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace 微山ASP.NETCore_LayUI开发框架
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
